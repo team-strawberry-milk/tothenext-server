1e8171a77692afae3e35733e3d13c05a6fe72f80
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-02 11:02:23 +0900

    chore: setting project structure

---
+++ src/main/java/com/berry/next/domains/application/HelloController.java
@@ -0,0 +1,16 @@
+package com.berry.next.domains.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Hello, To The Next Server / team strawberry-milk";
+    }
+}
