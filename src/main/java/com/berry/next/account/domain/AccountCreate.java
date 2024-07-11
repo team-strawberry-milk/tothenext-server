65cb440fb63014892b34a5769b4a874d4bd13188
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-12 02:54:34 +0900

    feat(#4): validate 라이브러리 추가

--- src/main/java/com/berry/next/account/domain/AccountCreate.java
+++ src/main/java/com/berry/next/account/domain/AccountCreate.java
@@ -1,9 +1,11 @@
 package com.berry.next.account.domain;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class AccountCreate {
-    private String email;
-    private String password;
+    @Email private String email;
+    @NotBlank private String password;
 }
