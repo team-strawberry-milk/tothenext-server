4a034cbd164377d91bd46f791faa22fc5ee4b4e4
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-16 03:04:41 +0900

    feat(#5): jwt 발급, 재발급, 인증 메서드 작성

---
+++ src/main/java/com/berry/next/security/domain/Token.java
@@ -0,0 +1,10 @@
+package com.berry.next.security.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public record Token(String accessToken, String refreshToken) {
+    @Builder
+    public Token {}
+}
