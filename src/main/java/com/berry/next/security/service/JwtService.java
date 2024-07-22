0a45eb1fdb51b68632f00cfda6b5984798e55421
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-23 01:09:08 +0900

    feat(#5): Cookie 기반 refresh token 발행 변경

--- src/main/java/com/berry/next/security/service/JwtService.java
+++ src/main/java/com/berry/next/security/service/JwtService.java
@@ -1,23 +1,45 @@
 package com.berry.next.security.service;
 
+import com.berry.next.redis.RedisUtil;
 import com.berry.next.security.domain.Token;
+import com.berry.next.security.util.CookieUtil;
 import com.berry.next.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
 import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class JwtService {
     private final JwtUtil jwtUtil;
+    private final CookieUtil cookieUtil;
+    private final RedisUtil redisUtil;
     private static final Long ACCESS_TOKEN_EXPIRE_TIME = Duration.ofHours(1).toMillis();
     private static final Long REFRESH_TOKEN_EXPIRE_TIME = Duration.ofDays(14).toMillis();
 
     public Token issue(Long id) {
+        String refreshToken = jwtUtil.generateToken(String.valueOf(id), REFRESH_TOKEN_EXPIRE_TIME);
+        redisUtil.setValues(jwtUtil.getTokenString(refreshToken), String.valueOf(id), REFRESH_TOKEN_EXPIRE_TIME);
+
         return Token.builder()
                 .accessToken(jwtUtil.generateToken(String.valueOf(id), ACCESS_TOKEN_EXPIRE_TIME))
-                .refreshToken(jwtUtil.generateToken(String.valueOf(id), REFRESH_TOKEN_EXPIRE_TIME))
+                .refreshToken(refreshToken)
                 .build();
+    }
+
+
+    public Token reissue(String bearerToken) {
+        String token = jwtUtil.getTokenString(bearerToken);
+        String id = redisUtil.getValues(token);
+        return Token.builder()
+                .accessToken(jwtUtil.generateToken(id, ACCESS_TOKEN_EXPIRE_TIME))
+                .build();
+    }
+
+    public ResponseCookie generateCookie(String token) throws UnsupportedEncodingException {
+        return cookieUtil.generateCookie(token);
     }
 }
---
+++ src/main/java/com/berry/next/security/util/CookieUtil.java
@@ -0,0 +1,22 @@
+package com.berry.next.security.util;
+
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class CookieUtil {
+    public ResponseCookie generateCookie(String token) throws UnsupportedEncodingException {
+        String encodedToken = URLEncoder.encode(token, StandardCharsets.UTF_8.toString());
+        return ResponseCookie
+                .from("refreshToken", encodedToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/accounts")
+                .build();
+    }
+}
