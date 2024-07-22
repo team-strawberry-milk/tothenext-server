a45e1fc4458e158e5f11d2e5f9139c63f9298147
gimye <gimyedev@gmail.com>
2024-07-22 22:58:11 +0900

    [feat] ActivityService 인터페이스 생성 #16

---
+++ src/main/java/com/berry/next/activity/domain/ActivityService.java
@@ -0,0 +1,20 @@
+package com.berry.next.activity.domain;
+
+import com.berry.next.account.domain.Account;
+
+import java.util.List;
+
+public interface ActivityService {
+
+    Activity createActivity(ActivityCreate create);
+
+    Activity modifyActivity(Account account,ActivityModify req);
+
+    void remove(Account account, Activity activity);
+
+    List<Activity> getAllActivities();
+
+    List<Activity> getRecentActivities();
+
+    Activity getActivityById(Long id);
+}
