c2b438f86a91c4f655a877f25085b2f493954679
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-27 13:29:53 +0900

    feat(#19): 활동 신청자 storage 클래스 생성

---
+++ src/main/java/com/berry/next/activity/storage/ActivityApplyEntity.java
@@ -0,0 +1,58 @@
+package com.berry.next.activity.storage;
+
+import com.berry.next.account.storage.AccountEntity;
+import com.berry.next.common.storage.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "activity_apply")
+@SQLDelete(sql = "UPDATE activity_apply SET is_deleted = true WHERE id = ?")
+@SQLRestriction("select * from activity_apply where is_deleted = false")
+public class ActivityApplyEntity extends BaseTimeEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account", nullable = false, insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @MapsId
+    private AccountEntity account;
+
+    @Column(name = "account", nullable = false)
+    private Long accountId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity", nullable = false, insertable = false, updatable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @MapsId
+    private ActivityEntity activity;
+
+    @Column(name = "activity", nullable = false)
+    private Long activityId;
+
+    @Column(name = "title", length = 512)
+    private String title;
+
+    @Column(name = "detail", columnDefinition = "text")
+    private String detail;
+
+    @ColumnDefault("0")
+    @Column(name = "is_accepted")
+    private Boolean isAccepted;
+
+    @ColumnDefault("0")
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Builder
+    public ActivityApplyEntity(Long accountId, Long activityId, String title, String detail, Boolean isAccepted, Boolean isDeleted) {
+        this.accountId = accountId;
+        this.activityId = activityId;
+        this.title = title;
+        this.detail = detail;
+        this.isAccepted = isAccepted;
+        this.isDeleted = isDeleted;
+    }
+}
